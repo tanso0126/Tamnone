ff62f87535429d722d107c3734ae94ed7ae642d1
Watson <dlckdals0126@naver.com>
2025-05-15 17:38:05 +0900

    강제로 전체 업로드

--- run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# 아래 코드 복사해서 셸에 붙여넣기
-python3 whisper_online_server.py --language ko --min-chunk-size 1 --vad --buffer_trimming_sec 1 --model medium
-
-# 아래 코드는 새로운 터미널 열어서 붙여넣기
-sox -t coreaudio default -b 16 -e signed -c 1 -r 16000 -t raw - | nc localhost 43007
